311b5193f5b9a730e55caf1acebe15e452ce6241
HamoudaAmine <amine.hamouda@gmail.com>
2020-03-02 10:38:47 +0100

    delete signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,6 @@ const routes: Routes = [
 		canLoad: [ AuthGuard ]
 	},
 	{ path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginModule) },
-	{ path: 'signup', loadChildren: () => import('./signup/signup.module').then((m) => m.SignupModule) },
 	{ path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then((m) => m.NotFoundModule) },
 	{ path: '**', redirectTo: 'not-found' }
 ];
